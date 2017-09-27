dd153966f47bf2a54554145ae1060ebee4a907d4
JTH <jhudspeth24@gmail.com>
2017-09-27 18:25:17 -0500

    Modified Vec3 to use generics instead of float

--- src/vec3.cpp
+++ src/vec3.cpp
@@ -1,56 +1,67 @@
 #include "iostream"
 #include "math.h"
-#include "../include/vec3.h"
+//#include "../include/vec3.h"
 
+template<typename T>
+class Vec3
+{
+public:
+    // 3 most basic ways of initializing a vector
+    Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
+    Vec3(const T &xx) : x(xx), y(xx), z(xx) {}
+    Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+    T x, y, z;
 
-vec3::vec3(float x,float y,float z)
-    : x{x}, y{y}, z{z}
-    {}
+    T length(){
+        return sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
+    }
 
-float vec3::length() {
-    return sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
-}
+    //This produces a normalized vector, meaning it has the same direction but unit length.
+    Vec3 norm() {
+        T len = this->length();
+        return Vec3(this->x /len, this->y /len, this->z /len);
+    }
 
-vec3 vec3::norm() {
-    float len = this->length();
-    return vec3(this->x /len, this->y /len, this->z /len);
-}
+    //Overloading the ostream to produce the vectors components.
+    friend std::ostream & operator << (std::ostream &os, const Vec3 &a) {
+        os << "x :" << a.x << " y :" << a.y << " z :" << a.z;
+        return os;
+    }
 
+    //Some basic operator overloading for vector arithmetic.
+    friend Vec3  operator+(const Vec3 &a,const Vec3 &b) {
+        return Vec3(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
 
-vec3  operator+(const vec3& a,const vec3& b) {
-    return vec3(a.x + b.x, a.y + b.y, a.z + b.z);
-}
+    friend Vec3 operator-(const Vec3& a,const Vec3& b) {
+        return Vec3(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
 
+    friend Vec3 operator*(T scalar,const Vec3 &a) {
+        return Vec3(scalar * a.x, scalar * a.y, scalar * a.z);
+    }
 
-vec3 operator-(const vec3& a,const vec3& b) {
-    return vec3(a.x - b.x, a.y - b.y, a.z - b.z);
-}
+    friend Vec3 operator*(const Vec3 &a, T scalar) {
+        return scalar * a;
+    }
+};
 
-
-vec3 operator*(float scalar,const vec3& a) {
-    return vec3(scalar*a.x, scalar*a.y, scalar*a.z);
-}
-
-
-vec3 operator*(const vec3& a, float scalar) {
-    return scalar * a;
-}
-
-
-std::ostream& operator<<(std::ostream& os, const vec3& a) {
-    os << "x :" << a.x << " y :" << a.y << " z :" << a.z;
-    return os;
-}
-/*
 int main() {
-    vec3 a = vec3(1,2,3);
-    std::cout << a;
-    vec3 b = a + a;
-    std::cout << "\n" << b;
-    vec3 unit = vec3(1.0f, 1.0f, 1.0f);
-    std::cout << '\n' << unit.length() ;
-    std::cout << '\n' << sqrt(3);
-    vec3 norm = unit.norm();
-    std::cout << unit.norm() ;
+    Vec3<int> a = Vec3<int>(1,2,3);
+    std::cout << "a: " << a <<"\n";
+    Vec3<float> b = Vec3<float>(3.0f,4.0f,5.0f);
+    std::cout << "b.length(): " << b.length() << "\n";
+    std::cout << "b.norm(): " << b.norm() << "\n";
+    std::cout << "b.norm().length(): " << b.norm().length() << "\n";
+    Vec3<int> c = a + a;
+    std::cout << "c: " << c << "\n";
+    Vec3<int> d = c - a;
+    std::cout << "d: " << d << "\n";
+    std::cout << "d * 5: " << d * 5 << "\n";
+    Vec3<float> unit = Vec3<float>(1.0f, 1.0f, 1.0f);
+    std::cout << "unit.length(): " << unit.length() << '\n';
+    std::cout << "sqrt(3): " << sqrt(3) << '\n';
+    Vec3<float> norm = unit.norm();
+    std::cout << "unit.norm(): " << unit.norm() << '\n';
+    std::cout << "unit.norm().length(): " << unit.norm().length() << '\n';
 }
-*/
